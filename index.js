a9a54b1f2810a509a7ba5feb497273dd7fc34007
e <12530451+sea-witch@users.noreply.github.com>
2024-05-13 22:21:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Disco!')
+  res.send('<body style="font-size: 5rem; color: hotpink; background: black;"><pre>synchat<pre></body>')
 })
 
 app.listen(port, () => {
