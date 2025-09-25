28bd903a43dabf2bd46b95428be49e55ff2050fc
Sea Witch <12530451+sea-witch@users.noreply.github.com>
2025-09-25 15:02:04 -0400

    .

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const pg = `<!doctype html>
+<html>
+  <head>
+    <title>This is the title of the webpage!</title>
+  </head>
+  <body>
+    <p>This is an example paragraph. Anything in the <strong>body</strong> tag will appear on the page, just like this <strong>p</strong> tag and its contents.</p>
+  </body>
+</html>`
+
 app.get('/', (req, res) => {
-  res.send('oh damn')
+	
+  res.send(req.body)
 })
 
 app.listen(port, () => {
