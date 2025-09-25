49eb1ed827bc8dc7446b216199a2dd7b292320d5
Sea Witch <12530451+sea-witch@users.noreply.github.com>
2025-09-25 15:20:13 -0400

    .

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const pg = `<!doctype html>
+<html>
+  <head>
+    <title>:)</title>
+  </head>
+  <body>
+    <p>Ahhhhh.</p>
+  </body>
+</html>`
+
 app.get('/', (req, res) => {
-  res.send('blurg')
+  res.send(pg)
 })
 
 app.listen(port, () => {
